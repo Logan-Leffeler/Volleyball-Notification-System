f0a4c2b6f7a3fa020909cc3591763eb370be62a7
Logan Leffeler <leffeler@gmail.com>
2023-05-08 22:21:43 -0500

    add tags to terraform resources

--- main.tf
+++ main.tf
@@ -4,10 +4,12 @@ provider "aws" {
 
 resource "aws_s3_bucket" "volleyball_spreadsheets" {
   bucket = "weekly-volleyball"
+  tags   = "Volleyball"
 }
 
 resource "aws_s3_bucket_versioning" "volleyball_spreadsheets_versioning" {
   bucket = aws_s3_bucket.volleyball_spreadsheets.id
+  tags   = "Volleyball"
 
   versioning_configuration {
     status = "Enabled"
@@ -16,27 +18,30 @@ resource "aws_s3_bucket_versioning" "volleyball_spreadsheets_versioning" {
 
 resource "aws_s3_object" "json_key" {
   depends_on = [ aws_s3_bucket.aws_s3_bucket.volleyball_spreadsheets, aws_s3_bucket_versioning.volleyball_spreadsheets_versioning ]
-  bucket = "weekly-volleyball"
-  key    = "virtual-equator-386019-d1063402b3b1.json"
-  source = "virtual-equator-386019-d1063402b3b1.json"
+  tags       = "Volleyball"
+  bucket     = "weekly-volleyball"
+  key        = "virtual-equator-386019-d1063402b3b1.json"
+  source     = "virtual-equator-386019-d1063402b3b1.json"
 }
 
 resource "aws_s3_object" "code_zip" {
   depends_on = [ aws_s3_bucket.aws_s3_bucket.volleyball_spreadsheets, aws_s3_bucket_versioning.volleyball_spreadsheets_versioning ]
-  bucket = "weekly-volleyball"
-  key    = "code_zip.zip"
-  source = "code_zip.zip"
+  tags       = "Volleyball"
+  bucket     = "weekly-volleyball"
+  key        = "code_zip.zip"
+  source     = "code_zip.zip"
 }
 
 resource "aws_iam_role" "lambda_execution_role" {
   name = "vball_lambda_execution_role"
+  tags = "Volleyball"
 
   assume_role_policy = jsonencode({
     Version = "2012-10-17"
     Statement = [
       {
-        Action = "sts:AssumeRole"
-        Effect = "Allow"
+        Action    = "sts:AssumeRole"
+        Effect    = "Allow"
         Principal = {
           Service = "lambda.amazonaws.com"
         }
@@ -46,9 +51,10 @@ resource "aws_iam_role" "lambda_execution_role" {
 }
 
 resource "aws_dynamodb_table" "volleyball_tracker" {
-  name = "volleyball_tracker"
+  name         = "volleyball_tracker"
+  tags         = "Volleyball"
   billing_mode = "PAY_PER_REQUEST"
-  hash_key = "id"
+  hash_key     = "id"
 
   attribute {
     name = "id"
@@ -58,12 +64,10 @@ resource "aws_dynamodb_table" "volleyball_tracker" {
 
 resource "aws_dynamodb_table_item" "example_item" {
   depends_on = [ aws_dynamodb_table.volleyball_tracker ]
+  tags       = "Volleyball"
   table_name = aws_dynamodb_table.volleyball_tracker.name
-
-  hash_key = aws_dynamodb_table.volleyball_tracker.hash_key
-
-
-  item = <<ITEM
+  hash_key   = aws_dynamodb_table.volleyball_tracker.hash_key
+  item       = <<ITEM
 {
   "id": {"S": "week_counter"},
   "current_week": {"N": "0"},
@@ -76,10 +80,10 @@ ITEM
 
 
 resource "aws_iam_policy" "lambda_execution_policy" {
-  name = "vball_lambda_execution_policy"
-
+  name   = "vball_lambda_execution_policy"
+  tags   = "Volleyball"
   policy = jsonencode({
-    Version = "2012-10-17"
+    Version   = "2012-10-17"
     Statement = [
       {
         Action = [
@@ -121,6 +125,7 @@ resource "aws_iam_policy" "lambda_execution_policy" {
 resource "aws_iam_role_policy_attachment" "lambda_execution_policy_attachment" {
   policy_arn = aws_iam_policy.lambda_execution_policy.arn
   role       = aws_iam_role.lambda_execution_role.name
+  tags       = "Volleyball"
 }
 
 resource "aws_lambda_function" "volleyball_tracker" {
@@ -136,5 +141,6 @@ resource "aws_lambda_function" "volleyball_tracker" {
   runtime          = "python3.8"
   timeout          = 30
   memory_size      = 128
+  tags             = "Volleyball"
 }
 
